91a38f17d39051d15e5bbb6e349e9ca1cb5877d7
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-13 10:33:38 -0700

    dec/reset

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -15,6 +15,10 @@ const counterReducer = (state, action) => {
       return { ...state, currentColor: action.payload.currentColor };
     case 'INCREMENT':
       return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    case 'RESET':
+      return { ...state, count: 0 };
   }
 };
 
